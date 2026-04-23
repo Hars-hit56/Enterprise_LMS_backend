4104b85857e13e3ff869c0ec2bd22a010dfbe929
Hars-hit56 <116793791+Hars-hit56@users.noreply.github.com>
2026-04-23 21:15:13 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -36,8 +36,8 @@ export const signUp = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -67,8 +67,8 @@ export const login = async (req, res) => {
     let token = await genToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
